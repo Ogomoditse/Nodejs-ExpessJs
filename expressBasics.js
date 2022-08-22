def3376279e52f5dd6999d86b03f61c667c9ef56
PCG Software <pcgsoftware@PCGs-Mac-mini.local>
2022-08-22 16:40:32 +0200

    22/08/2022

---
+++ expressBasics.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express()
+
+app.get('/',(req,res)=>{
+    console.log("Hit it")
+    res.status(404).send('Home Page')
+})
+
+app.get('/about', (req,res) => {
+
+    res.send('About Page')
+})
+
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>resource not found</>')
+})
+
+app.listen(5000,()=>{
+    console.log('server is listening on port 5000')
+})
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
